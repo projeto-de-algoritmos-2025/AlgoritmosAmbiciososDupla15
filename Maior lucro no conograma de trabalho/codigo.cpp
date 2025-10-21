9de8fd236b2550cadcf9caeb2b4917347ac3a7a7
arthur-suares <arthur.lantr@gmail.com>
2025-10-20 21:10:53 -0300

    Ultimo execicio

---
+++ Maior lucro no conograma de trabalho/codigo.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = startTime.size();
+        vector<array<int, 3>> jobs(n);
+        
+        for (int i = 0; i < n; ++i) {
+            jobs[i] = {endTime[i], startTime[i], profit[i]};
+        }
+        
+       
+        sort(jobs.begin(), jobs.end());
+        
+        
+        vector<int> dp(n);
+        dp[0] = jobs[0][2]; 
+        
+        for (int i = 1; i < n; ++i) {
+            int currProfit = jobs[i][2];
+            
+            int l = 0, r = i - 1, lastNonOverlap = -1;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (jobs[mid][0] <= jobs[i][1]) {
+                    lastNonOverlap = mid;
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            
+            if (lastNonOverlap != -1)
+                currProfit += dp[lastNonOverlap];
+            
+            dp[i] = max(dp[i - 1], currProfit);
+        }
+        
+        return dp[n - 1];
+    }
+};
